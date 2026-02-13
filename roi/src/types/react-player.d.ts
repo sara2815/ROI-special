8673f83967f4c2560ce8afd219acc2a8f418d343
Sara <asinghde@student.ubc.ca>
2026-02-13 01:17:33 -0800

    strcuture more

--- roi/src/types/react-player.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'react-player' {
-  import * as React from 'react';
-  type ReactPlayerProps = Record<string, any>;
-  const ReactPlayer: React.ComponentType<ReactPlayerProps>;
-  export default ReactPlayer;
-}
